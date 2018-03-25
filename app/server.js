376aa8eb23e436ecad60c94aad3030cb581b6fbb
HueyNemud <bertranddumenieu@gmail.com>
2018-03-25 19:31:58 +0200

    Move the app core to /app

---
+++ app/server.js
@@ -0,0 +1,46 @@
+
+/**
+* Module Dependencies
+*/
+const config = require('./config');
+const restify = require('restify');
+const pgp = require('pg-promise');
+
+/**
+* Initialize Server
+*/
+const server = restify.createServer({
+  name: config.name,
+  version: config.version,
+});
+
+
+/**
+* Load plugins
+*/
+server.use(restify.plugins.queryParser());
+
+
+/**
+*  Middleware
+*/
+server.pre((req, res, next) => {
+  console.info(`${req.method} - ${req.url} - ${req.query}`);
+  return next();
+});
+
+/**
+*  Requests handling
+*/
+server.get('/geocode', (req, res, next) => {
+  res.send(200,{ok: 'true'});
+  return next();
+});   
+
+
+
+server.listen(config.port, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
+
+module.exports = server;
--- test/server.js
+++ test/server.js
@@ -1,10 +1,12 @@
 const request = require('supertest');
-const geocoder = require('../index.js')
+const geocoder = require('../app/server.js')
 
 describe('GET /geocode', function() {
-  it('respond to GET /geocode', function(done) {
+  it('GET /geocode should respond and send JSON', function(done) {
     request(geocoder)
       .get('/geocode')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
       .expect(200, done);
   });
 });
