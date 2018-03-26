2c65a2c8cba909cc5fca65cf310bdb1920f8a597
HueyNemud <bertranddumenieu@gmail.com>
2018-03-26 22:15:31 +0200

    Define a controller to hendle geocoding

---
+++ app/controllers/geocoder.controller.js
@@ -0,0 +1,59 @@
+class GeocoderController {
+
+  constructor(){};
+
+  create(){};
+
+  retrieve(params = {adress: 'paris', date: '2018', precise: '1', maxresults: '1' }, model = {}, callback = () => {}){
+    let sql=`SELECT rank::text,
+            $<query_addr>||';'||$<query_date> AS input_adresse_query,
+            historical_name::text,
+            normalised_name::text,
+            sfti2daterange(COALESCE(f.specific_fuzzy_date,hs.default_fuzzy_date)) AS fuzzy_date,
+            CASE WHEN ST_NumGeometries(geom2) =1 
+              THEN ST_AsText(ST_GeometryN(geom2,1)) 
+              ELSE ST_AsText(geom2) 
+            END AS geom,
+            historical_source::text,
+            numerical_origin_process::text,
+            historical_geocoding.round(aggregated_distance::float,6) AS aggregated_distance,
+            historical_geocoding.round(spatial_precision::float,2) AS spatial_precision,
+            historical_geocoding.round(confidence_in_result::float,3) AS confidence_in_result,
+            historical_geocoding.round(semantic_distance::float,3 ) AS semantic_distance,
+            historical_geocoding.round(temporal_distance::float,3) AS temporal_distance,
+            historical_geocoding.round(number_distance::float,3) AS number_distance,
+            historical_geocoding.round(scale_distance::float,3) AS scale_distance,
+            historical_geocoding.round(spatial_distance::float,3) AS spatial_distance  
+      FROM historical_geocoding.geocode_name_foolproof(
+                                                        query_adress:=$<query_addr>,
+                                                        query_date:= sfti_makesfti($<query_date>::integer),
+                                                        use_precise_localisation:= $<do_precise_geocoding>::integer::boolean ,
+                                                        max_number_of_candidates:=$<max_results>::integer
+                                                      ) As f
+      LEFT OUTER JOIN geohistorical_object.historical_source AS hs 
+      ON (hs.short_name = f.historical_source),
+          ST_SnapToGrid(geom,0.01) AS geom2
+      ;`;
+
+    model.any(sql,
+    {
+        query_addr: params.address,
+        query_date: params.date,
+        do_precise_geocoding: params.precise,
+        max_results: params.maxresults,
+    })
+    .then(data => {
+      callback({ code: 200, data: data });
+    })
+    .catch(error => {
+        console.log('ERROR:', error);
+    });
+  };
+
+  update(){};
+
+  delete(){};
+};
+
+
+module.exports = new GeocoderController;
--- app/server.js
+++ app/server.js
@@ -1,10 +1,12 @@
 const restify = require('restify');
+const errors = require('restify-errors');
+const ctrlGeocoder = require('./controllers/geocoder.controller.js');
 const pgp = require('pg-promise')({});
 
 class GeocoderServer {
   
   constructor(cfg = { name: 'default', version: '1.0', db: {} }){
-    
+
     this._server =  restify.createServer({
       name: cfg.name,
       version: cfg.version
@@ -13,7 +15,7 @@ class GeocoderServer {
     this._server.use(restify.plugins.queryParser());
 
     this._server.pre((req, res, next) => {
-      console.info(`${req.method} - ${req.url} - ${req.query}`);
+      console.info(`${req.method} - ${req.url}`);
       return next();
     });
 
@@ -28,57 +30,19 @@ class GeocoderServer {
 
   _createRoutes(){
 
-    this._server.get({url: '/geocode'},(req, res, next) => {
-      let sql=`SELECT rank::text,
-                  $<query_addr>||';'||$<query_date> AS input_adresse_query,
-                  historical_name::text,
-                  normalised_name::text,
-                  sfti2daterange(COALESCE(f.specific_fuzzy_date,hs.default_fuzzy_date)) AS fuzzy_date,
-                  CASE WHEN ST_NumGeometries(geom2) =1 
-                    THEN ST_AsText(ST_GeometryN(geom2,1)) 
-                    ELSE ST_AsText(geom2) 
-                  END AS geom,
-                  historical_source::text,
-                  numerical_origin_process::text,
-                  historical_geocoding.round(aggregated_distance::float,6) AS aggregated_distance,
-                  historical_geocoding.round(spatial_precision::float,2) AS spatial_precision,
-                  historical_geocoding.round(confidence_in_result::float,3) AS confidence_in_result,
-                  historical_geocoding.round(semantic_distance::float,3 ) AS semantic_distance,
-                  historical_geocoding.round(temporal_distance::float,3) AS temporal_distance,
-                  historical_geocoding.round(number_distance::float,3) AS number_distance,
-                  historical_geocoding.round(scale_distance::float,3) AS scale_distance,
-                  historical_geocoding.round(spatial_distance::float,3) AS spatial_distance  
-            FROM historical_geocoding.geocode_name_foolproof(
-                                                              query_adress:=$<query_addr>,
-                                                              query_date:= sfti_makesfti($<query_date>::integer),
-                                                              use_precise_localisation:= $<do_precise_geocoding>::integer::boolean ,
-                                                              max_number_of_candidates:=$<max_results>::integer
-                                                            ) As f
-            LEFT OUTER JOIN geohistorical_object.historical_source AS hs 
-            ON (hs.short_name = f.historical_source),
-                ST_SnapToGrid(geom,0.01) AS geom2
-            ;`;
-      console.info(this._database);
-      this._database.any(sql,
-        {
-            query_addr: req.query.address,
-            query_date: req.query.date,
-            do_precise_geocoding: req.query.precise,
-            max_results: req.query.maxresults,
-        })
-        .then(data => {
-            res.send(200,data);
-        })
-        .catch(error => {
-            console.log('ERROR:', error); // print the error;
-        });
+    this._server.get('/geocode', (req, res, next) => {
+      //TODO Avoid passing the db connection to controllers on every call.
+      ctrlGeocoder.retrieve(req.query, this._database, (response) => { res.send(response.code, response.data) }); 
       return next();
-    });   
+    });
 
+    this._server.post('/geocode', (req,res,next) => {
+      return next( new  errors.BadRequestError());  
+    });
   };
 };
 
-// Classes aren't hoisted so we need to wrap them in a function.
-module.exports = module.exports = {
+// Classes aren't hoisted so we need to wrap them in a function unless we return an instance.
+module.exports = {
   GeocoderServer: GeocoderServer
 };
