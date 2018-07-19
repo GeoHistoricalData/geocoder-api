1345716269339aa68cd1fd18c472e64dc6893599
HueyNemud <bertranddumenieu@gmail.com>
2018-07-19 15:52:32 +0200

    Returns geometries with lat/long coordinates

--- db/sql/geocode_address.sql
+++ db/sql/geocode_address.sql
@@ -3,10 +3,8 @@ SELECT rank::text,
           historical_name::text,
           normalised_name::text,
           sfti2daterange(COALESCE(f.specific_fuzzy_date,hs.default_fuzzy_date)) AS fuzzy_date,
-          CASE WHEN ST_NumGeometries(geom2) =1 
-            THEN ST_AsText(ST_GeometryN(geom2,1)) 
-            ELSE ST_AsText(geom2) 
-          END AS geom,
+          ST_AsGeoJSON(geom2)::json as geometry,
+          ST_AsGeoJSON(geog)::json as geography,
           historical_source::text,
           numerical_origin_process::text,
           historical_geocoding.round(aggregated_distance::float,6) AS aggregated_distance,
@@ -17,7 +15,7 @@ SELECT rank::text,
           historical_geocoding.round(number_distance::float,3) AS number_distance,
           historical_geocoding.round(scale_distance::float,3) AS scale_distance,
           historical_geocoding.round(spatial_distance::float,3) AS spatial_distance  
-    FROM historical_geocoding.geocode_name_base(
+    FROM historical_geocoding.geocode_name_latlong(
                                                       query_adress:=$<query_addr>,
                                                       query_date:= sfti_makesfti($<query_date>::integer),
                                                       use_precise_localisation:= $<do_precise_geocoding>::integer::boolean ,
